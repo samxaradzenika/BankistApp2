218fad8dd444e8d85219991b7b55e8a03b78ffab
samxaradzenika <murdoc746@gmail.com>
2023-02-27 20:11:02 +0400

    practiced, applied Math methods and rounding

---
+++ practice.js
@@ -0,0 +1,32 @@
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 22, 3, 12));
+console.log(Math.max(5, `22`, 3, 12));
+console.log(Math.max(5, `22px`, 3, 12));
+
+console.log(Math.min(5, 22, 3, 12));
+
+console.log(Math.PI * Number.parseFloat(`10px`) ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
+
+//Rounding Ints
+console.log(Math.trunc(23.3));
+console.log(Math.trunc(23.9));
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
